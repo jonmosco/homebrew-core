fa9de6b1a02c1e530605b39f43942d305274e1cf
Jon Mosco <jonny.mosco@gmail.com>
2018-01-16 10:41:19 -0500

    new release

--- Formula/kube-ps1.rb
+++ Formula/kube-ps1.rb
@@ -1,8 +1,8 @@
 class KubePs1 < Formula
   desc "Kubernetes prompt info for bash and zsh"
   homepage "https://github.com/jonmosco/kube-ps1"
-  url "https://github.com/jonmosco/kube-ps1/archive/0.1.0.tar.gz"
-  sha256 "fdff921bcc37417070f18d05c2173b8fae7bbb79daa3d35ad5cda4b9394067eb"
+  url "https://github.com/jonmosco/kube-ps1/archive/0.4.0.tar.gz"
+  sha256 "0133c5b41609ab8b4e0715f6581a8ee006a979028ddd697bab7a28c02f903db5"
   head "https://github.com/jonmosco/kube-ps1"
 
   bottle :unneeded
@@ -21,7 +21,7 @@ class KubePs1 < Formula
 
       For Bash:
       source "#{opt_prefix}/kube-ps1.sh"
-      PS1='[\\u@\h \W\[$(kube_ps1)\]]\$ $PS1'
+      PS1="[\u@\h \W \$(kube_ps1)]\$ "
     EOS
   end
 
