6b046a700c4c5bb3b61e3ca8b3ae23704b8ac337
Jon Mosco <jonny.mosco@gmail.com>
2018-02-19 16:26:16 -0500

    remove existence test

--- Formula/kube-ps1.rb
+++ Formula/kube-ps1.rb
@@ -27,7 +27,6 @@ class KubePs1 < Formula
 
   test do
     # Test that the file exists
-    assert_predicate testpath/"#{opt_prefix}/kube-ps1.sh", :exist?
     kubeon = ". #{opt_prefix}/kube-ps1.sh && echo $KUBE_PS1_SHELL"
     assert_equal "zsh", shell_output("zsh -c '#{kubeon}'").chomp
   end
