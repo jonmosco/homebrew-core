48e482d48dffc91859d787d25c7ce9e3b3f327df
ilovezfs <ilovezfs@icloud.com>
2018-02-23 22:53:42 +0000

    Update kube-ps1.rb

--- Formula/kube-ps1.rb
+++ Formula/kube-ps1.rb
@@ -1,8 +1,8 @@
 class KubePs1 < Formula
   desc "Kubernetes prompt info for bash and zsh"
   homepage "https://github.com/jonmosco/kube-ps1"
-  url "https://github.com/jonmosco/kube-ps1/releases/tag/0.5.0"
-  sha256 "fe5484d4a7e453aebd3ab64d98992858cb3dd97e"
+  url "https://github.com/jonmosco/kube-ps1/archive/0.5.0.tar.gz"
+  sha256 "ac0ecfb5f943a40d36743aba4179efe4af0b9b204c99fb6d31c4239f356e34dd"
   head "https://github.com/jonmosco/kube-ps1.git"
 
   bottle :unneeded
